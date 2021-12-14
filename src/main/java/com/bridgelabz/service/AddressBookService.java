b1cc6aa44165b7c3324fe73d1484b36e342a4da3
Sudip Panja <panja370@gmail.com>
2021-12-14 22:44:47 +0530

    Added : Develeoper and Testing application properties for implementing the spring profile framework for address book service application

--- src/main/java/com/bridgelabz/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/service/AddressBookService.java
@@ -7,14 +7,11 @@ import com.bridgelabz.entity.AddressBook;
 import com.bridgelabz.exception.custom.AddressBookNotFoundException;
 import com.bridgelabz.exception.custom.BadRequestException;
 import com.bridgelabz.repository.AddressBookRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Purpose : To demonstrate business logic which implements all the methods in controller layer
