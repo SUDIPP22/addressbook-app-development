08ca861c9c91e647713ac13a89678216680be608
Sudip Panja <panja370@gmail.com>
2021-12-14 10:20:23 +0530

    Added : Documentation comments

--- src/main/java/com/bridgelabz/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/AddressBookApplication.java
@@ -3,11 +3,18 @@ package com.bridgelabz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Purpose : To demonstrate address book application
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-13
+ */
 @SpringBootApplication
 public class AddressBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
 
 }
