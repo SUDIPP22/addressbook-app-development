a91a41c9744444dd6da31aca2b331bca660f19e3
Sudip Panja <panja370@gmail.com>
2021-12-15 05:46:18 +0530

    Added : AddressBookControllerIntegrationTest class to integrate the control layer with MockMvc framework for address book application

---
+++ src/test/java/com/bridgelabz/integration/AddressBookControllerIntegrationTest.java
@@ -0,0 +1,85 @@
+package com.bridgelabz.integration;
+
+import com.bridgelabz.controller.AddressBookController;
+import com.bridgelabz.dto.AddressBookDTO;
+import com.bridgelabz.dto.ResponseDTO;
+import com.bridgelabz.service.AddressBookService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(AddressBookController.class)
+@ActiveProfiles("test")
+public class AddressBookControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AddressBookService addressBookService;
+
+    @Test
+    void addAddressBookTest() throws Exception {
+        when(addressBookService.addAddressBook(any())).thenReturn(new ResponseDTO());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/address")
+                        .content("{\"firstName\":\"Sudip\",\"lastName\":\"Panja\"," +
+                                "\"address\":\"1/A/7,RamlalAgarWalaLane\",\"city\":\"Kolkata\"," +
+                                "\"state\":\"West Bengal\",\"phoneNumber\":\"91 8910211371\",\"zipCode\":700050}")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getAddressDetails() throws Exception {
+        when(addressBookService.getAddressDetails()).thenReturn(new ArrayList<>());
+        mockMvc.perform(MockMvcRequestBuilders.get("/addresses"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateAddressBookTest() throws Exception {
+        AddressBookDTO addressBookDTO = new AddressBookDTO();
+        addressBookDTO.setFirstName("Naruto");
+        addressBookDTO.setLastName("Uzumaki");
+        addressBookDTO.setAddress("Uzumaki Clan");
+        addressBookDTO.setCity("Hidden Leaf Village");
+        addressBookDTO.setState("Konohagakure");
+        addressBookDTO.setPhoneNumber("91 8334003795");
+        addressBookDTO.setZipCode("100026");
+        int addressId = 1;
+        when(addressBookService.updateAddressBook(addressId, addressBookDTO)).thenReturn(new ResponseDTO());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/address/1")
+                        .content("{\"firstName\":\"Sudip\",\"lastName\":\"Panja\"," +
+                                "\"address\":\"1/A/7,RamlalAgarWalaLane\",\"city\":\"Kolkata\"," +
+                                "\"state\":\"West Bengal\",\"phoneNumber\":\"91 8910211371\",\"zipCode\":700050}")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    void deleteAddressBookTest() throws Exception {
+        when(addressBookService.deleteAddressBookById(1)).thenReturn(new ResponseDTO());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/address/1")
+                        .content("{\"firstName\":\"Sudip\",\"lastName\":\"Panja\"," +
+                                "\"address\":\"1/A/7,RamlalAgarWalaLane\",\"city\":\"Kolkata\"," +
+                                "\"state\":\"West Bengal\",\"phoneNumber\":\"91 8910211371\",\"zipCode\":700050}")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isAccepted());
+    }
+}
